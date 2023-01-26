a0779af59501c81d0e5f6a0afb5b50d76362d13b
Aral011 <aral.imankulov@icloud.com>
2023-01-26 12:13:02 +0600

    new test

---
+++ main.py
@@ -0,0 +1,47 @@
+#JSON
+
+# import json
+# d = {'hello': True, 'age':2, 'name': None}
+# json_obj = json.dumps(d)
+# print(json_obj)
+# python_obj = json.loads(json_obj)
+# print(python_obj)
+
+
+
+# import json
+# with open('data.json', 'r') as file:
+#     python_obj = json.load(file)     #load and dump работает с файлом, а loads and dumps со строкой
+#     print(python_obj)
+
+
+
+
+
+# СУБД -система управления база данными
+#PostgreSQL - SQL запросы, база данных это хранилища данными, хранится в табличках, у таблиц есть поля, тип поля,
+# связь многие ко многим (создание третий таблицы)
+
+
+
+
+
+
+
+
+#ООП - объктно ориентированное программирование, способ написания кода
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#GIT, PostgreSQL, (http,orm)
